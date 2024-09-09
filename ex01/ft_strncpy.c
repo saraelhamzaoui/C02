228bd6f885e780fd66ce267d38cca036623e3502
Sara El Hamzaoui <elhamzaouisarah10@gmail.com>
2024-09-09 13:21:39 +0100

    ex01 is done

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,33 +6,34 @@
 /*   By: sel-hamz <sel-hamz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 12:14:43 by sel-hamz          #+#    #+#             */
-/*   Updated: 2024/09/09 12:41:34 by sel-hamz         ###   ########.fr       */
+/*   Updated: 2024/09/09 13:21:21 by sel-hamz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
-
-// char *ft_strncpy(char *dest, char *src, unsigned int n)
-// {
-//     int i;
-    
-    
-// }
-
-void    sdjchkd(int n)
+#include <stdio.h>
+char *ft_strncpy(char *dest, char *src, unsigned int n)
 {
-    // n : 2
     int i;
+
     i = 0;
-    while (n)
+    while (i < n && src[i])
     {
-        printf("Hi\n");
-        n--;
+        dest[i] = src[i];
+        i++; 
     }
+    if (src[i] == '\0')
+        dest[i] = src[i];
+    return (dest);
 }
 
 int main()
 {
-    
+    char source[7] = "az";
+    char destination[10];
+    ft_strncpy(destination, source, 10);
+    printf("%s\n", destination);
+    // int i = 0;
+    // while (destination[i])
     return (0);
 }
