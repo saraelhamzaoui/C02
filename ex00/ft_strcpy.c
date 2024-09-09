966bac9da6182da2e323dbca4235a28111477b64
Sara El Hamzaoui <elhamzaouisarah10@gmail.com>
2024-09-09 12:12:11 +0100

    ex00 is done

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,7 +6,35 @@
 /*   By: sel-hamz <sel-hamz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 10:37:49 by sel-hamz          #+#    #+#             */
-/*   Updated: 2024/09/09 10:37:50 by sel-hamz         ###   ########.fr       */
+/*   Updated: 2024/09/09 12:11:40 by sel-hamz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+}
+
+/*
+int    main()
+{
+    char *s1 = "Abc"; // 100
+    char s2[10];
+
+    ft_strcpy(s2, s1);
+    int i = 0;
+    while (s2[i])
+    {
+      write(1, &s2[i], 1);
+      i++;
+    }
+}
+*/
