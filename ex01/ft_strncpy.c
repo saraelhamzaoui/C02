7467e251cfb31f8178fae8ea914ff0352e05c078
Sara El Hamzaoui <elhamzaouisarah10@gmail.com>
2024-09-09 12:43:16 +0100

    Fix strcpy

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -1,41 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sel-hamz <sel-hamz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/09 10:37:49 by sel-hamz          #+#    #+#             */
-/*   Updated: 2024/09/09 12:42:58 by sel-hamz         ###   ########.fr       */
+/*   Created: 2024/09/09 12:14:43 by sel-hamz          #+#    #+#             */
+/*   Updated: 2024/09/09 12:41:34 by sel-hamz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char *dest, char *src)
+#include <string.h>
+
+// char *ft_strncpy(char *dest, char *src, unsigned int n)
+// {
+//     int i;
+    
+    
+// }
+
+void    sdjchkd(int n)
 {
-	int	i;
-
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-  return (dest);
-}
-
-/*
-int    main()
-{
-    char *s1 = "Abc"; // 100
-    char s2[10];
-
-    ft_strcpy(s2, s1);
-    int i = 0;
-    while (s2[i])
+    // n : 2
+    int i;
+    i = 0;
+    while (n)
     {
-      write(1, &s2[i], 1);
-      i++;
+        printf("Hi\n");
+        n--;
     }
 }
-*/
+
+int main()
+{
+    
+    return (0);
+}
